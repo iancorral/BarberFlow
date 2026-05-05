ce0921aa9a8a5b4f55888dba0dfdd2627a27afc2
ITIT <itit@iMacc-de-ITIT-5.local>
2026-05-05 15:56:01 -0600

    First commit

---
+++ BarberFlow/BarberFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarberFlowApp.swift
+//  BarberFlow
+//
+//  Created by ITIT on 05/05/26.
+//
+
+import SwiftUI
+
+@main
+struct BarberFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarberFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BarberFlow
+//
+//  Created by ITIT on 05/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
